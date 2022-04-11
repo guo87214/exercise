efa1596358a5df639bf4e17aafc37719a33f9e38
guolichao <guo87214@163.com>
2022-04-12 00:01:42 +0800

    summary 439

--- thinkinginjava/src/test/holding/NonCollectionSequence.java
+++ thinkinginjava/src/test/holding/NonCollectionSequence.java
@@ -1,6 +1,11 @@
 package test.holding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
 
 import typeinfo.pets.Pet;
 import typeinfo.pets.Pets;
@@ -9,32 +14,83 @@ class PetSequence{
 	protected Pet[] pets=Pets.createArray(8);
 }
 public class NonCollectionSequence extends PetSequence{
-	public Iterator<Pet> iterator(){
-		return new Iterator<Pet>() {
-			private int index=0;
-			
-			@Override
-			public boolean hasNext() {
-				// TODO Auto-generated method stub
-				return index<pets.length;
-			}
+	public Iterable iterable() {
+		return new Iterable() {
 
 			@Override
-			public Pet next() {
+			public Iterator iterator() {
 				// TODO Auto-generated method stub
-				return pets[index++];
-			}
+				return new Iterator() {
+					int i;
+					@Override
+					public boolean hasNext() {
+						// TODO Auto-generated method stub
+						return i<pets.length;
+					}
+
+					@Override
+					public Object next() {
+						// TODO Auto-generated method stub
+						return pets[i++];
+					}
+
+					@Override
+					public void remove() {
+						// TODO Auto-generated method stub
+						
+					}
+					
+				};
+			}};
+		
+	}
+	public List Randomized () {
+		Random random=new Random(34);
+		List list=new ArrayList(Arrays.asList(pets));
+		Collections.shuffle(list, random);
+		return list;
+		
+	}
+	public List CollReversed() {
+		List list=new ArrayList(Arrays.asList(pets));
+		Collections.reverse(list);
+		return list;
+		
+	}
+	
+	public Iterable reversed() {
+		
+		return new Iterable() {
 
 			@Override
-			public void remove() {
+			public Iterator iterator() {
 				// TODO Auto-generated method stub
-				throw new UnsupportedOperationException();
-			}
-			
-		};
+				return new Iterator() {
+					int i=pets.length-1;
+					@Override
+					public boolean hasNext() {
+						// TODO Auto-generated method stub
+						return i>=0;
+					}
+
+					@Override
+					public Object next() {
+						// TODO Auto-generated method stub
+						return pets[i--];
+					}
+
+					@Override
+					public void remove() {
+						// TODO Auto-generated method stub
+						
+					}
+					
+				};
+			}};
+		
 	}
-	public static void main(String[] args) {
-		NonCollectionSequence nc=new NonCollectionSequence();
-		InterfaceVsIterator.display(nc.iterator());
-	}
+	/*
+	 * public static void main(String[] args) { NonCollectionSequence nc=new
+	 * NonCollectionSequence(); InterfaceVsIterator.display(nc.iterable()); }
+	 */
 }
