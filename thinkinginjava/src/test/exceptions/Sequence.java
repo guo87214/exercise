d6b216c2f4904a6b302a3a87002b5e845fa8c8fc
guolichao <guo87214@163.com>
2022-05-10 00:00:10 +0800

    what's finally for? 473

---
+++ thinkinginjava/src/test/exceptions/Sequence.java
@@ -0,0 +1,44 @@
+//: innerclasses/Sequence.java
+// Holds a sequence of Objects.
+package test.exceptions;
+interface Selector {
+  boolean end();
+  Object current();
+  void next();
+}	
+
+public class Sequence {
+  private Object[] items;
+  private int next = 0;
+  public Sequence(int size) { items = new Object[size]; }
+  public void add(Object x) {
+    
+      items[next++] = x;
+  }
+  private class SequenceSelector implements Selector {
+    private int i = 0;
+    public boolean end() { return i == items.length; }
+    public Object current() { return items[i]; }
+    public void next() { if(i < items.length) i++; }
+  }
+  public Selector selector() {
+    return new SequenceSelector();
+  }	
+  public static void main(String[] args) {
+    Sequence sequence = new Sequence(10);
+    for(int i = 0; i < 10; i++) {
+      sequence.add(Integer.toString(i));
+    }
+    sequence.add("133");
+    sequence.add("133");
+    sequence.add("133");
+    sequence.add("133");
+    Selector selector = sequence.selector();
+    while(!selector.end()) {
+      System.out.print(selector.current() + " ");
+      selector.next();
+    }
+  }
+} /* Output:
+0 1 2 3 4 5 6 7 8 9
+*///:~
